3d85c079065d3925bb625daef868132db95ff9e6
Kristian Freeman <kristianfreeman@gmail.com>
2012-03-02 10:04:21 -0800

    Initial commit

--- xcodelaunch/AppDelegate.h
+++ xcodelaunch/AppDelegate.h
@@ -11,5 +11,12 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSButton *Prod;
+@property (weak) IBOutlet NSButton *Dev;
+@property (weak) IBOutlet NSTextField *prodLoc;
+@property (weak) IBOutlet NSTextField *devLoc;
+
+- (IBAction)chooseDev:(id)sender;
+- (IBAction)chooseProd:(id)sender;
 
 @end
