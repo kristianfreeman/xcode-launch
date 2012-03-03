13b45b4638e8841b675b1957da2b9285beaae805
Jack Lawrence <jlawrence@mail.rit.edu>
2012-03-03 18:02:49 -0500

    Sectioned constants.

--- xcodelaunch/JLConstants.h
+++ xcodelaunch/JLConstants.h
@@ -8,14 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+/** NSUserDefaults Keys */
 static NSString *const JLUserDeclinedLoginItemUserDefault = @"JLUserDeclinedLoginItem";
 static NSString *const JLDockIconIsHiddenUserDefault = @"DockIconIsHidden";
 
+/** NSNotificationCenter Keys */
 static NSString *const JLXcodeWasLaunchedNotification = @"JLXcodeWasLaunchedNotification";
 
+/** Application Paths */
 static NSString *const JLProductionPath = @"/Developer/Applications/Xcode.app";
-
 static NSString *const JLDevelopmentPath = @"/Applications/Xcode.app";
 
+/** Application Versions */
 static NSString *const JLProductionVersion = @"5.0";
 static NSString *const JLDevelopmentVersion = @"5.1";
