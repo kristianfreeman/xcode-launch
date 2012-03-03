5ceb1e0b20ac473870827d76601ecc90ba130cb5
Jack Lawrence <jlawrence@mail.rit.edu>
2012-03-03 04:18:46 -0500

    Application now attempts to add itself to the login items.

--- xcodelaunch/AppDelegate.m
+++ xcodelaunch/AppDelegate.m
@@ -10,17 +10,77 @@
 
 #import "JLStatusBarIcon.h"
 
+@interface AppDelegate ()
+
+- (void)enableLoginItemWithLoginItemsReference:(LSSharedFileListRef)theLoginItemsRefs ForPath:(NSString *)appPath;
+- (BOOL)loginItemExistsWithLoginItemReference:(LSSharedFileListRef)theLoginItemsRefs ForPath:(NSString *)appPath;
+
+@end
+
 @implementation AppDelegate
 
 @synthesize statusBarItem = _statusBarItem;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+    
+    dispatch_async(queue, ^{
+        NSString * appPath = [[NSBundle mainBundle] bundlePath];
+        
+        LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL, kLSSharedFileListSessionLoginItems, NULL);
+        if (![self loginItemExistsWithLoginItemReference:loginItems ForPath:appPath]) {
+            [self enableLoginItemWithLoginItemsReference:loginItems ForPath:appPath];
+        }
+        CFRelease(loginItems);
+    });
+
     CGFloat thickness = [[NSStatusBar systemStatusBar] thickness];
     _statusBarItem = [[NSStatusBar systemStatusBar] statusItemWithLength:thickness];
     JLStatusBarIcon *statusBarIcon = [[JLStatusBarIcon alloc] initWithFrame:NSMakeRect(0, 0, thickness, thickness)];
     [self.statusBarItem setView:statusBarIcon];
     [self.statusBarItem setHighlightMode:YES]; 
+}
+
+- (void)enableLoginItemWithLoginItemsReference:(LSSharedFileListRef )theLoginItemsRefs ForPath:(NSString *)appPath {
+    // We call LSSharedFileListInsertItemURL to insert the item at the bottom of Login Items list.
+    CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:appPath];
+    
+    CFMutableDictionaryRef inPropertiesToSet = CFDictionaryCreateMutable(NULL, 1, NULL, NULL);
+    CFDictionaryAddValue(inPropertiesToSet, kLSSharedFileListLoginItemHidden, kCFBooleanTrue);
+    
+    LSSharedFileListItemRef itemRef = LSSharedFileListInsertItemURL(theLoginItemsRefs, kLSSharedFileListItemLast, NULL, NULL, url, inPropertiesToSet, NULL);       
+    if (itemRef) {
+        CFRelease(itemRef);
+    }
+    if (inPropertiesToSet) {
+        CFRelease(inPropertiesToSet);
+    }
+}
+
+- (BOOL)loginItemExistsWithLoginItemReference:(LSSharedFileListRef)theLoginItemsRefs ForPath:(NSString *)appPath {
+	BOOL found = NO;  
+	UInt32 seedValue;
+	CFURLRef thePath = NULL;
+    
+	// We're going to grab the contents of the shared file list (LSSharedFileListItemRef objects)
+	// and pop it in an array so we can iterate through it to find our item.
+	CFArrayRef loginItemsArray = LSSharedFileListCopySnapshot(theLoginItemsRefs, &seedValue);
+	for (id item in (__bridge NSArray *)loginItemsArray) {    
+		LSSharedFileListItemRef itemRef = (__bridge LSSharedFileListItemRef)item;
+		if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &thePath, NULL) == noErr) {
+			if ([[(__bridge NSURL *)thePath path] hasPrefix:appPath]) {
+				found = YES;
+				break;
+			}
+            // Docs for LSSharedFileListItemResolve say we're responsible
+            // for releasing the CFURLRef that is returned
+            if (thePath != NULL) CFRelease(thePath);
+		}
+	}
+	if (loginItemsArray != NULL) CFRelease(loginItemsArray);
+    
+	return found;
 }
 
 
